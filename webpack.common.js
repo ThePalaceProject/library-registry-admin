e85de4582afa86bdcc2586fa49e875618b0f4787
Ray Lee <ray.lee@lyrasis.org>
2023-01-04 18:15:15 -0500

    Update all package scripts for Node 18.

--- webpack.common.js
+++ webpack.common.js
@@ -26,12 +26,6 @@ var config = {
       "process.env.TEST_AXE": JSON.stringify(process.env.TEST_AXE)
     })
   ],
-  optimization: {
-    minimizer: [new TerserPlugin({
-      terserOptions: { compress: false },
-      sourceMap: true
-    })]
-  },
   module: {
     rules: [
       {
@@ -45,16 +39,22 @@ var config = {
       {
         test: /\.tsx?$/,
         exclude: [/node_modules/],
-        loaders: ['ts-loader']
+        use: ["ts-loader"]
       },
       {
         test: /\.(png|woff|woff2|eot|ttf|svg).*$/,
-        loader: 'url-loader?limit=100000'
+        use: ["url-loader?limit=100000"]
       }
     ],
   },
   resolve: {
     extensions: [".ts", ".tsx", ".js", ".scss"],
+    fallback: {
+      buffer: require.resolve("buffer"),
+      stream: require.resolve("stream-browserify"),
+      timers: require.resolve("timers-browserify"),
+      url: require.resolve("url")
+    }
   }
 };
 
