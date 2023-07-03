52350fcc7f7b296e65bddce4b941d8c2e8032b0b
Tim DiLauro <tdilauro@users.noreply.github.com>
2023-07-03 16:09:35 -0400

    Refactor `Charts` to functional component.

--- src/components/Charts.tsx
+++ src/components/Charts.tsx
@@ -9,56 +9,49 @@ export interface ChartsProps {
 }
 
 /** The "Charts" tab in the "Aggregate Data" panel.  Uses the external "recharts" package. */
-export default class Charts extends React.Component<ChartsProps, {}> {
-  static defaultProps = {
-    width: "50%",
-    height: 400
+export default (props: ChartsProps) => {
+  const {data, width = "50%", height = 400} = props;
+  const colors = {
+    "production": "#809F69",
+    "testing": "#FFCD61",
+    "cancelled": "#DA5D62"
   };
+  let chartData = Object.keys(data).map((category) => {
+    return { "name": category, "value": data[category].length };
+  });
 
-  render() {
-    const colors = {
-      "production": "#809F69",
-      "testing": "#FFCD61",
-      "cancelled": "#DA5D62"
-    };
-    const { width, height } = this.props;
-    let chartData = Object.keys(this.props.data).map((category) => {
-      return { "name": category, "value": this.props.data[category].length };
-    });
-
-    return (
-        <div className="chart-view">
-          <ResponsiveContainer width={width} height={height}>
-            <PieChart ref="pie-chart-svg">
-              <Legend
-                payload={ chartData.map(
-                  item => ({
-                    id: item.name,
-                    type: "diamond",
-                    value: item.name,
-                    color: colors[item.name],
-                  })
-                )}
-                layout="vertical"
-                height={60}
-                width={200}
-                align="center"
-                verticalAlign="bottom"
-              />
-              <Pie label data={chartData} dataKey="value" nameKey="name" cx="50%" cy="50%" outerRadius={100} />
-              <Tooltip />
-            </PieChart>
-          </ResponsiveContainer>
-          <ResponsiveContainer width={width} height={height}>
-            <BarChart ref="bar-chart-svg" width={400} height={400} data={chartData}>
-              <CartesianGrid strokeDasharray="5 5" />
-              <XAxis dataKey="name" interval={0} />
-              <YAxis />
-              <Tooltip />
-              <Bar dataKey="value" barSize={40} />
-            </BarChart>
+  return (
+      <div className="chart-view">
+        <ResponsiveContainer width={width} height={height}>
+          <PieChart>
+            <Legend
+              payload={ chartData.map(
+                item => ({
+                  id: item.name,
+                  type: "diamond",
+                  value: item.name,
+                  color: colors[item.name],
+                })
+              )}
+              layout="vertical"
+              height={60}
+              width={200}
+              align="center"
+              verticalAlign="bottom"
+            />
+            <Pie label data={chartData} dataKey="value" nameKey="name" cx="50%" cy="50%" outerRadius={100} />
+            <Tooltip />
+          </PieChart>
         </ResponsiveContainer>
-      </div>
-    );
-  }
-}
+        <ResponsiveContainer width={width} height={height}>
+          <BarChart width={400} height={400} data={chartData}>
+            <CartesianGrid strokeDasharray="5 5" />
+            <XAxis dataKey="name" interval={0} />
+            <YAxis />
+            <Tooltip />
+            <Bar dataKey="value" barSize={40} />
+          </BarChart>
+      </ResponsiveContainer>
+    </div>
+  );
+};
